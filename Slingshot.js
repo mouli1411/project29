0c06a9368475ae0d171161dec53f232ae0a3e9e8
mouli1411 <77237426+mouli1411@users.noreply.github.com>
2021-01-25 15:29:41 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,20 @@
+class Slingshot{
+    constructor(bodyA, bodyB){
+        var options = {
+            bodyA: bodyA,
+            bodyB: bodyB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.slingShot = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+    display(){
+        var pointA = this.slingShot.bodyA.position;
+        var pointB = this.slingShot.bodyB.position;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+
+}
